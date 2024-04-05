c6ea5c3c4061479a97b456daaa6e241643be3575
KSpasov <kspasov@infragistics.com>
2024-04-05 23:04:32 +0300

    ReportDbModel created

---
+++ DataBase/DbModels/ReportDbModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.DbModels
+{
+    public class ReportDbModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string IdReport { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public byte [] Image { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        private ReportDbModel() { }
+
+        public ReportDbModel(string title, byte [] image, string description, string location)
+        {
+            this.Title = title;
+            this.Image = image;
+            this.Description = description;
+            this.Location = location;
+        }
+        public ReportDbModel(string title, string description, string location)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Location = location;
+        }
+
+    }
+}
