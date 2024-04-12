5e97c859b15fab23b8397e69c700bf4b4d93f027
ivanivanm <94964103+ivanivanm@users.noreply.github.com>
2024-04-12 07:56:39 +0200

    Added Models folder in DataAccessLayer. Copied the code for User model from another branch. Inserted the code for Comment model

---
+++ DataAccessLayer/Models/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Comment
+    {
+        public Comment(int id, int reportId, int userId, DateTime postedOn, string commentContent)
+        {
+            Id = id;
+            ReportId = reportId;
+            UserId = userId;
+            PostedOn = postedOn;
+            CommentContent = commentContent;
+        }
+
+        public int Id { get; set; }
+        public int ReportId { get; set; }
+        public int UserId { get; set; }
+        public DateTime PostedOn { get; set; }
+        public string CommentContent { get; set; }
+    }
+}
