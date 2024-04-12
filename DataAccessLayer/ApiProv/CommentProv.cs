70acc4a0d298a296575e10301c96f708049d955f
ivanivanm <94964103+ivanivanm@users.noreply.github.com>
2024-04-12 17:12:08 +0200

    Added ApiProv folder. Copied the UserProv code from another branch and added CommentProv.

---
+++ DataAccessLayer/ApiProv/CommentProv.cs
@@ -0,0 +1,92 @@
+﻿using DataAccessLayer.DbProv;
+using DataAccessLayer.Models;
+using DataBase.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ApiProv
+{
+    public class CommentProv
+    {
+        private readonly CommentContext _context;
+        private readonly UserContext _userContext;
+
+        public CommentProv(CommentContext context, UserContext userContext)
+        {
+            _context = context;
+            _userContext = userContext;
+        }
+        public async Task CreateComment(Comment comment)
+        {
+            if (comment == null)
+            {
+                throw new NullReferenceException("comment is null");
+            }
+
+            UserDbModel commentUser = await _userContext.Read(comment.UserId);
+
+            if (commentUser is null)
+            {
+                throw new NullReferenceException("user is null");
+            }
+
+            CommentDbModel newComment = new CommentDbModel(comment.UserId, comment.ReportId, comment.PostedOn, comment.CommentContent, commentUser);
+
+            await _context.Create(newComment);
+        }
+        public async Task<Comment> ReadComment(int key)
+        {
+            CommentDbModel commentFromDb = await _context.Read(key, true, false);
+
+            if (commentFromDb is null)
+            {
+                throw new NullReferenceException("Comment with such a key doesn't exist");
+            }
+
+            Comment comment = new Comment(commentFromDb.Id, commentFromDb.UserId, commentFromDb.ReportId, commentFromDb.PostedOn, commentFromDb.CommentContent);
+
+            return comment;
+        }
+        public async Task<List<Comment>> ReadAllComments()
+        {
+            List<CommentDbModel> commentsFromDb = await _context.ReadAll(true, false);
+
+            if (commentsFromDb is null)
+            {
+                throw new NullReferenceException("Our app doesn't have comments");
+            }
+
+            List<Comment> comments = commentsFromDb.
+                Select(cd => new Comment(cd.Id, cd.ReportId, cd.UserId, cd.PostedOn, cd.CommentContent)).ToList();
+
+            return comments;
+        }
+        public async Task UpdateComment(Comment comment)
+        {
+            if (comment is null)
+            {
+                throw new NullReferenceException("Comment doesn't exist");
+            }
+
+            if (comment.UserId == 0)
+            {
+                throw new NullReferenceException("User doesn't exist");
+            }
+
+            UserDbModel userFromDb = await _userContext.Read(comment.UserId);
+
+            CommentDbModel dbComment = new CommentDbModel
+                (comment.UserId, comment.ReportId, comment.PostedOn, comment.CommentContent, userFromDb);
+            dbComment.Id = comment.Id;
+
+            await _context.Update(dbComment, true);
+        }
+        public async Task DeleteComment(int key)
+        {
+            _context.Delete(key);
+        }
+    }
+}
---
+++ DataAccessLayer/ApiProv/UserProv.cs
@@ -0,0 +1,72 @@
+﻿using DataAccessLayer.DbProv;
+using DataAccessLayer.Models;
+using DataBase.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ApiProv
+{
+    public class UserProv
+    {
+        private readonly UserContext _userContext;
+
+        public UserProv(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public async Task CreateUser(User user)
+        {
+            if (user == null)
+            {
+                throw new Exception("user was null");
+            }
+
+            UserDbModel newUser = new UserDbModel(user.Name, user.Email, user.Password, user.Role);
+            await _userContext.Create(newUser);
+        }
+
+        public async Task<User> ReadUser(int key)
+        {
+            UserDbModel userFromDb = await _userContext.Read(key);
+            User user = new()
+            {
+                Id = userFromDb.Id,
+                Name = userFromDb.Name,
+                Email = userFromDb.Email,
+                Password = userFromDb.Password,
+                Role = userFromDb.Role
+            };
+            return user;
+        }
+
+        public async Task<List<User>> ReadAllUsers()
+        {
+            List<UserDbModel> usersFromDb = await _userContext.ReadAll();
+            List<User> users = new List<User>();
+
+            users = usersFromDb.Select(x => new User
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Email = x.Email,
+                Password = x.Password,
+                Role = x.Role
+            }).ToList();
+            return users;
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            UserDbModel userToBeUpdated = new UserDbModel(user.Name, user.Email, user.Password, user.Role);
+            await _userContext.Update(userToBeUpdated);
+        }
+        public async Task DeleteUser(int key)
+        {
+            await _userContext.Delete(key);
+        }
+    }
+}
